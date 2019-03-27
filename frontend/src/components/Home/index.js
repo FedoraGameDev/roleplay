1afa011eea061138f2f6d42ef70e7aca2ed453ae
Jimmy Satterfield <fedora.game.dev@gmail.com>
2019-03-27 02:30:16 -0700

    Storing username in state at Home

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -2,8 +2,17 @@ import React from "react";
 import { withAuthorization } from "../firebase/Session";
 import axios from "axios";
 
+const INITIAL_STATE = {
+    username: null
+}
+
 class HomePage extends React.Component
 {
+    componentWillMount()
+    {
+        this.setState({ ...INITIAL_STATE });
+    }
+
     constructor(props)
     {
         super(props);
@@ -11,7 +20,7 @@ class HomePage extends React.Component
         axios.post("http://localhost:3001/user", { token: localStorage.token })
             .then(res =>
             {
-                //
+                this.setState({ username: res.data.data.username });
             })
             .catch(error =>
             {
@@ -24,6 +33,7 @@ class HomePage extends React.Component
         return (
             <div>
                 <h1>Home Page</h1>
+                <p>Hello {this.state.username}</p>
             </div>
         );
     }
