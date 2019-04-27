92167764ac859e345873c2677bc0ee686d443d31
Jimmy Satterfield <fedora.game.dev@gmail.com>
2019-04-27 01:23:28 -0700

    Adding react build

---
+++ frontend/build/precache-manifest.bcd087f36abbba40c1405fa9703f41fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c00db32f333f3fd370eb5ef60334874",
+    "url": "/static/media/bookCover.9c00db32.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dedad2c4d83015a07417",
+    "url": "/static/js/main.5c93be68.chunk.js"
+  },
+  {
+    "revision": "e0feb858214ad9668f2b",
+    "url": "/static/js/2.92322cb8.chunk.js"
+  },
+  {
+    "revision": "dedad2c4d83015a07417",
+    "url": "/static/css/main.85d3b7ad.chunk.css"
+  },
+  {
+    "revision": "e0feb858214ad9668f2b",
+    "url": "/static/css/2.82b3a277.chunk.css"
+  },
+  {
+    "revision": "b17ad0e6afc91e5c0a237f11dd71b3b1",
+    "url": "/index.html"
+  }
+];
